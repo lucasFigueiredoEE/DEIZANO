6975f8c94c8e7cfcad7e41db3b512174139a2209
lucas.figueiredo <lucas.figueiredo@ee.ufcg.edu.br>
2019-09-27 12:22:12 -0300

    Mais comandos

--- DEIZANO/DEIZANO.py
+++ DEIZANO/DEIZANO.py
@@ -1,11 +1,46 @@
-print('D.E.I.Z.A.N.O.')
+import time
 import speech_recognition as sr
-from pathlib import Path
+
+print('D.E.I.Z.A.N.O.')
+
 r = sr.Recognizer()
-harvard = sr.WavFile("TestAudio/harvard.wav")
-with harvard as source:
-    audio = r.record(source)
-try:
-    print("Transcription: " + r.recognize_google(audio))   # recognize speech using Google Speech Recognition
-except LookupError:                                 # speech is unintelligible
-    print("Could not understand audio")
+#print(sr.Microphone.list_microphone_names())
+mic = sr.Microphone(device_index=1)
+
+while True:
+    print('Fale um comando ...')
+    with mic as source:
+        audio = r.listen(source,timeout=5,phrase_time_limit=5)
+
+    try:
+        entrada = r.recognize_google(audio,language="pt-BR")
+        #print("Você disse: " + entrada)
+        if '10 anos' in entrada:
+            #print('Trabalhando ...')
+            if 'ligar a luz' in entrada:
+                if 'quarto' in entrada:
+                    print('Luz do quarto acesa') # comando NodeRED -> MQTT
+                elif 'cozinha' in entrada:
+                    print('Luz da cozinha acesa')
+                elif 'terraço' in entrada:
+                    print('Luz do terraço acesa')
+                else:
+                    print('Não sei onde é isso, Major')
+            elif 'desligar a luz' in entrada:
+                if 'quarto' in entrada:
+                    print('Luz do quarto apagada') # comando NodeRED -> MQTT
+                elif 'cozinha' in entrada:
+                    print('Luz da cozinha apagada')
+                elif 'terraço' in entrada:
+                    print('Luz do terraço apagada')
+                else:
+                    print('Não sei onde é isso, Major')
+            elif 'boa noite':
+                print('Valeu, Major')
+            else:
+                print('Sei fazer isso não, Major')
+        else:
+            print("Como é, Major?")
+    except:                                 # speech is unintelligible
+        print("Como é, Major?")
+    time.sleep(5)
