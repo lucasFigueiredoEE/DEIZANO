912f91f8972a277e0391886d1238e21cf445f2a2
lucas.figueiredo <lucas.figueiredo@ee.ufcg.edu.br>
2019-09-27 07:41:56 -0300

    Transcrição a partir de arquivo funcionando

--- DEIZANO/DEIZANO.py
+++ DEIZANO/DEIZANO.py
@@ -1,3 +1,11 @@
-
+print('D.E.I.Z.A.N.O.')
 import speech_recognition as sr
+from pathlib import Path
 r = sr.Recognizer()
+harvard = sr.WavFile("TestAudio/harvard.wav")
+with harvard as source:
+    audio = r.record(source)
+try:
+    print("Transcription: " + r.recognize_google(audio))   # recognize speech using Google Speech Recognition
+except LookupError:                                 # speech is unintelligible
+    print("Could not understand audio")
